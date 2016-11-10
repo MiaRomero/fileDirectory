f9efdc98b14eee7bf47a2cc425ae15ffceda6c1b
Maria Romero <mariaromero18e@gmail.com>
2016-11-10 12:25:04 -0800

    add views folder

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 cosnt app = express();
-const filesRouter = require('/filesRouter');
+const filesRouter = require(__dirname + '/filesRouter');
 
 app.get('/hello', (req, res) => {
   res.send('<h2>Hello World</h2>');
 });
 
 app.use('/files', filesRouter);
+
+app.use(express.static(__dirname + '/views'));
+
 app.use('/*', (req, res) => {
   res.status(404).send('<h2>404 Not Found</h2>');
 });
