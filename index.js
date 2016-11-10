83af81047df9e0ea4290154a965a7e9c06e03993
Maria Romero <mariaromero18e@gmail.com>
2016-11-10 14:06:57 -0800

    added route provider

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express');
 cosnt app = express();
-const filesRouter = require(__dirname + '/filesRouter');
 
-app.get('/hello', (req, res) => {
-  res.send('<h2>Hello World</h2>');
+const angular = require('angular');
+const filesApp = angular.module('filesApp', ['ngRoute']);
+
+const fs = require('fs');
+
+
+filesApp.config(function($routeProvider) {
+  $routeProvider
+
+    .when('/files', {
+      templateUrl: 'views/index.html',
+      controller: 'listController'
+    })
+    .when('/hello', {
+      templateUrl: 'views/hello.html',
+      //controller: ''
+    })
+    .when('/proc', {
+      templateUrl: 'views/proc.html',
+      controller: 'procController'
+    });
 });
 
-app.use('/files', filesRouter);
+filesApp.controller('listController', [listResource], function(Resource) {
+  this.files = [];
 
-app.use(express.static(__dirname + '/views'));
-
-app.use('/*', (req, res) => {
-  res.status(404).send('<h2>404 Not Found</h2>');
+  this.getNextLevel = function(startPath) {
+      fs.readdir(startPath, (err, files) => {
+        for(var i = 0; i < files.length; i++) {
+          console.log(startPath + files[i]);
+          var path = startPath + files[i];
+          console.log(fs.statSync(path).isDirectory());
+        }
+      });
+    };
 });
 
 app.listen(3000, () => {
